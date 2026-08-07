695e428f4c76063eaaa3debb71ceb5351b0e4fb1
Niraj Mandaliya <nirajmandaliya@gmail.com>
2026-08-07 16:39:09 +0530

    update: homework and video footer UI adjustments

--- wp-content/plugins/tfp-dashboard/includes/page-week.php
+++ wp-content/plugins/tfp-dashboard/includes/page-week.php
@@ -347,11 +347,7 @@ function tfp_dashboard_render_week_homework_tab($week, $user_id)
                 </div>
                 <?php endforeach; ?>
             </div>
-            <div class="tfp-week__homework-sidebar-footer">
-                <!-- Global Submit Homework Button -->
-                <button class="tfp-dash-btn tfp-reded-btn tfp-homework-submit-btn tfp-homework-global-submit" style="width: 100%; margin-bottom: 12px;"><?php esc_html_e('Submit Homework', 'tfp-dashboard'); ?></button>
-                <a href="<?php echo esc_url($reading_url); ?>" class="tfp-dash-btn tfp-dash-btn--outline tfp-week__homework-back" style="width: 100%; text-align: center; justify-content: center;"><svg xmlns="http://www.w3.org/2000/svg" width="5" height="8" viewBox="0 0 5 8" fill="none"><path d="M4.93994 0.94L1.88661 4L4.93994 7.06L3.99994 8L-5.88141e-05 4L3.99994 -4.10887e-08L4.93994 0.94Z" fill="currentColor"/></svg> <?php esc_html_e('Back to Reading', 'tfp-dashboard'); ?></a>
-            </div>
+           
         </div>
         
         <div class="tfp-week__homework-main">
@@ -468,7 +464,14 @@ function tfp_dashboard_render_week_homework_tab($week, $user_id)
                 </div>
             </div>
 
+             <div class="tfp-week__homework-sidebar-footer">
+                <!-- Global Submit Homework Button -->
+                <button class="tfp-dash-btn tfp-reded-btn tfp-homework-submit-btn tfp-homework-global-submit"><?php esc_html_e('Submit Homework', 'tfp-dashboard'); ?></button>
+                <a href="<?php echo esc_url($reading_url); ?>" class="tfp-dash-btn tfp-dash-btn--outline tfp-week__homework-back"><svg xmlns="http://www.w3.org/2000/svg" width="5" height="8" viewBox="0 0 5 8" fill="none"><path d="M4.93994 0.94L1.88661 4L4.93994 7.06L3.99994 8L-5.88141e-05 4L3.99994 -4.10887e-08L4.93994 0.94Z" fill="currentColor"/></svg> <?php esc_html_e('Back to Reading', 'tfp-dashboard'); ?></a>
+            </div>
+
         </div>
+        
     </div>
     <?php
 }
